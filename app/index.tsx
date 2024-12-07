e93ed9b64e3f508f7fd43937136b89db002f1063
adildotes <adildotes@gmail.com>
2024-12-07 19:33:07 +0500

    more images added

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,16 @@ const imagesArr = [
     "https://images.unsplash.com/photo-1423666639041-f56000c27a9a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vYmlsZSUyMGFwcHN8ZW58MHx8MHx8fDA%3D",
     "https://images.unsplash.com/photo-1593789198788-8b21805d5fdb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG1vYmlsZSUyMGFwcHN8ZW58MHx8MHx8fDA%3D",
     "https://images.unsplash.com/photo-1533022139390-e31c488d69e2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vYmlsZSUyMGFwcHN8ZW58MHx8MHx8fDA%3D",
+    "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1518770660439-4636190af475?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1542744095-291d1f67b221?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1506765515384-028b60a970df?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1593642532400-2682810df593?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1517433456452-f9633a875f6f?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1526045612212-70caf35c14df?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1518770660439-4636190af475?w=800&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60",
 ];
 
 export default function ImageGallery() {
